e1232811dd02e72d80ab33ab5af2e254cb296d0f
khalidMujahid <binkhalid267@gmail.com>
2023-01-09 04:01:37 +0100

    Updated

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 mongoose
+  .set("strictQuery",true)
   .connect(process.env.MONGO_URL)
   .then(() => console.log("Database connected!"))
   .catch((error) => console.log(error));
